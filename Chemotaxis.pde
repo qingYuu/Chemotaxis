814372162fb681c712c3bd96e41b8f5913d64304
unknown <xiyu5@697-334-Frodo.sf.k12.internal>
2015-09-22 11:35:20 -0700

    add lights

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,5 +1,6 @@
 
  Bacteria [] mana;
+ Bubble [] tenshi;
  PImage img;
  //declare bacteria variables here   
  void setup()   
@@ -7,11 +8,16 @@
  	//initialize bacteria variables here 
  	size(1000, 1000);
     img = loadImage("kh.png");
- 	mana = new Bacteria[100]; 
+ 	mana = new Bacteria[50]; 
  	for(int i= 0; i<mana.length; i++)
  	{
  		mana[i]=new Bacteria(20,50);
  	}
+ 	tenshi = new Bubble[100];
+ 	for(int q= 0; q<tenshi.length; q++)
+    {
+    	tenshi[q]=new Bubble();
+    }
  	frameRate(10);
  }   
  void draw()   
@@ -23,6 +29,12 @@
     mana[m].move();
     mana[m].show();
     }
+    for(int n=0; n<tenshi.length; n++)
+    {
+    tenshi[n].move();
+    tenshi[n].show();
+    }
+
  	//move and show the bacteria   
  }  
  class Bacteria    
@@ -57,9 +69,7 @@
        if (mY >= mouseY) 
          {
            mY += (int)(Math.random()*7)-5;
-         }
-      
-       
+         }   
 
  	} 
  	void show()
@@ -69,5 +79,30 @@
  		ellipse(mX,mY,15,15);
  		stroke(225, 225, 225);
 		ellipse(mX,mY,10,10);
+ 	}
+ }
+
+ class Bubble
+ {
+ 	int aX, aY, aC;
+ 	Bubble()
+ 	{
+ 		aX = (int)(Math.random()*1000)-30;
+ 		aY = (int)(Math.random()*1000)-30;
+ 		aC = color((int)(Math.random()*255)-1, (int)(Math.random()*225)-1, (int)(Math.random()*255)-1,100);
+
+ 	}
+ 	void move()
+ 	{
+ 		aX += (int)(Math.random()*3)-1;
+        aY += (int)(Math.random()*3)-1;
+ 	}
+ 	void show()
+ 	{
+        noStroke();
+ 		fill(aC);
+ 		ellipse(aX,aY,15,15);
+ 		stroke(225, 225, 225);
+		ellipse(aX,aY,10,10);
  	}
  }
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -15,19 +15,42 @@ import java.io.IOException;
 public class Chemotaxis extends PApplet {
 
 
- Bacteria mana;
+ Bacteria [] mana;
+ Bubble [] tenshi;
+ PImage img;
  //declare bacteria variables here   
  public void setup()   
  {     
  	//initialize bacteria variables here 
- 	size(400, 400);
- 	mana = new Bacteria(40,40); 
+ 	size(1000, 1000);
+    img = loadImage("kh.png");
+ 	mana = new Bacteria[50]; 
+ 	for(int i= 0; i<mana.length; i++)
+ 	{
+ 		mana[i]=new Bacteria(20,50);
+ 	}
+ 	tenshi = new Bubble[100];
+ 	for(int q= 0; q<tenshi.length; q++)
+    {
+    	tenshi[q]=new Bubble();
+    }
  	frameRate(10);
  }   
  public void draw()   
- {    
-    mana.move();
-    mana.show();
+ {  
+ 	//background(0, 0, 0); 
+    image(img,0,0);
+ 	for(int m=0; m<mana.length; m++)
+ 	{
+    mana[m].move();
+    mana[m].show();
+    }
+    for(int n=0; n<tenshi.length; n++)
+    {
+    tenshi[n].move();
+    tenshi[n].show();
+    }
+
  	//move and show the bacteria   
  }  
  class Bacteria    
@@ -38,20 +61,65 @@ public class Chemotaxis extends PApplet {
 	{
 		mX = x;
 		mY = y;
-		//mC = fill(202, 232, 198);
+		mC = color((int)(Math.random()*255)-1, (int)(Math.random()*225)-1, (int)(Math.random()*255)-1,100);
 	}
 	public void move()
 	{
-       mX += (int)(Math.random()*2)+1;
-       mY += (int)(Math.random()*3)+2;
-
+      /* mX += (int)(Math.random()*3)-1;
+       mY += (int)(Math.random()*3)-1;
+       */
+       if ( mX <= mouseX )
+	       {
+	       	 mX += (int)(Math.random()*7)+5;
+	       } 
+       if (mX >= mouseX)
+		   {
+		     mX += (int)(Math.random()*7)-5;
+		   }
+  	  
+       if (mY <= mouseY )
+         {
+           mY += (int)(Math.random()*7)+5;
+       
+         }  
+       if (mY >= mouseY) 
+         {
+           mY += (int)(Math.random()*7)-5;
+         }   
 
  	} 
  	public void show()
+ 	{   
+ 		noStroke();
+ 		fill(mC);
+ 		ellipse(mX,mY,15,15);
+ 		stroke(225, 225, 225);
+		ellipse(mX,mY,10,10);
+ 	}
+ }
+
+ class Bubble
+ {
+ 	int aX, aY, aC;
+ 	Bubble()
  	{
- 		background(0, 0, 0);
- 		fill(202, 232, 198);
-		ellipse(mX,mY,30,30);
+ 		aX = (int)(Math.random()*1000)-30;
+ 		aY = (int)(Math.random()*1000)-30;
+ 		aC = color((int)(Math.random()*255)-1, (int)(Math.random()*225)-1, (int)(Math.random()*255)-1,100);
+
+ 	}
+ 	public void move()
+ 	{
+ 		aX += (int)(Math.random()*3)-1;
+        aY += (int)(Math.random()*3)-1;
+ 	}
+ 	public void show()
+ 	{
+        noStroke();
+ 		fill(aC);
+ 		ellipse(aX,aY,15,15);
+ 		stroke(225, 225, 225);
+		ellipse(aX,aY,10,10);
  	}
  }
   static public void main(String[] passedArgs) {
